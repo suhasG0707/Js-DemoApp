be50a9cf9bebeb0e1ab55646e2b778646bd757fb
suhas goilkar <152413835+suhasG0707@users.noreply.github.com>
2026-05-19 17:07:56 +0000

    coverssion off data  type

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,19 @@
-console.log("Suhas")
+//console.log("Suhas")
+//console.log(typeof undefined);// undefined type is undefined 
+//console.log(typeof null)//object
+let score=undefined
+let valueInNumber= Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+ //33 //convert esaly 
+ //"33abc"// convert to show Nan (not an number)
+ // true => 1
+ //null =>0
+ //undefined=> Nan
+
+ let valuesomenumber=45
+
+ let stringNumber= String(valuesomenumber)
+ console.log(stringNumber);
+ 
